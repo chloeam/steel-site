84c91bec06583de441d7c0ba82259f7ccb97957e
Chloe Atchue-Mamlet <chloe793@gmail.com>
2016-10-24 13:56:30 -0400

    finished header

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+  $('#burger').click(function(){
+    $(this).toggleClass('open');
+
+    if ( $('.main-nav') .is(':hidden')) {
+      $('.main-nav').slideDown(500);
+    } else {
+      $('.main-nav').slideUp(200);
+    }
+  });
+
 });
